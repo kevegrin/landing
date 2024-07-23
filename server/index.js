20ed6e094ae3aafea23413ada7627ba44790e71e
Pellegrin <kevin.pellegrinv@gmail.com>
2024-07-23 00:50:22 -0700

    initial setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// TODO add connection string
+mongoose.connect('your_mongoDB_connection_string', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch((error) => console.error(error.message));
